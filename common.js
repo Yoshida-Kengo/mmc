23c9478c8cdab37b0dfab9376e19adff59cd686e
Yoshida-Kengo <k.yoshida@micoworks.jp>
2025-01-08 18:18:39 +0900

    構成変更
    ファイル階層の変更と、共通となる処理を共通化

--- common.js
+++ common.js
@@ -9,14 +9,39 @@ function clearCookies() {
         d.shift();
       }
     }
-  }
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+    }
+    
+
+function deleteCookieEverywhere(name) {
+    var paths = [
+        '/',          // ルートパス
+        '/path1',     // 推定される他のパス1
+        '/path2',     // 推定される他のパス2
+        // 必要に応じて他の可能性のあるパスを追加
+    ];
+    
+    var domains = [
+        window.location.hostname, // 現在のホスト名
+        // 必要に応じて他の関連するドメインを追加
+    ];
+
+    paths.forEach(function(path) {
+        domains.forEach(function(domain) {
+            document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=' + path + '; domain=' + domain + ';';
+        });
+        // ドメイン未指定（現在のサブドメインを含むすべてのサブドメインを試す）
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=' + path + ';';
+    });
+}
+
 
 function cashClear() {
     clearCookies();
     clearLocalStorage();
+    deleteCookieEverywhere('myCookie');
     alert('キャッシュがクリアされました！');
-}
-
-function clearLocalStorage() {
-localStorage.clear();
 }
