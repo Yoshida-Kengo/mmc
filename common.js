d4aa31ebb5875552bd99d01ba1e8693d9fc84eff
Yoshida-Kengo <k.yoshida@micoworks.jp>
2025-01-08 18:51:35 +0900

    不要な処理の削除

--- common.js
+++ common.js
@@ -13,47 +13,20 @@ function clearCookies() {
 
 function clearLocalStorage() {
     localStorage.clear();
-    }
-    
-
-function deleteCookieEverywhere(name) {
-    var paths = [
-        '/',          // ルートパス
-        '/path1',     // 推定される他のパス1
-        '/path2',     // 推定される他のパス2
-        // 必要に応じて他の可能性のあるパスを追加
-    ];
-    
-    var domains = [
-        window.location.hostname, // 現在のホスト名
-        // 必要に応じて他の関連するドメインを追加
-    ];
-
-    paths.forEach(function(path) {
-        domains.forEach(function(domain) {
-            document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=' + path + '; domain=' + domain + ';';
-        });
-        // ドメイン未指定（現在のサブドメインを含むすべてのサブドメインを試す）
-        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=' + path + ';';
-    });
 }
+    
 
-function deleteAllCookies() {
-    const cookies = document.cookie.split(";");
-
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf("="); 
-        const name = eqPos > -1 ? cookie.slice(0, eqPos).trim() : cookie.trim();
-        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    }
+function deleteCookies(deleteList) {
+    deleteList.forEach(function(name) {
+        document.cookie = 'name= ' + name + '; max-age=0;';
+    });
 }
 
 
 function cashClear() {
     clearCookies();
     clearLocalStorage();
+    deleteCookies(["mc_token", "mc_customerid"]);
     // deleteCookieEverywhere('myCookie');
-    deleteAllCookies();
     alert('キャッシュがクリアされました！');
 }
