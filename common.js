37cdd1181b9d8f86a34196e496838e30f7c1d6d3
Yoshida-Kengo <k.yoshida@micoworks.jp>
2025-01-08 18:29:11 +0900

    クッキー削除に関する処理

--- common.js
+++ common.js
@@ -38,10 +38,22 @@ function deleteCookieEverywhere(name) {
     });
 }
 
+function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("="); 
+        const name = eqPos > -1 ? cookie.slice(0, eqPos).trim() : cookie.trim();
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    }
+}
+
 
 function cashClear() {
     clearCookies();
     clearLocalStorage();
-    deleteCookieEverywhere('myCookie');
+    // deleteCookieEverywhere('myCookie');
+    deleteAllCookies();
     alert('キャッシュがクリアされました！');
 }
